63817dd2ea75cc2bf0786d7716a041944ca4443c
Tanmay Nagepatil <brainsniper2408@gmail.com>
2022-01-10 13:57:25 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,6 @@
-
 cd /etc/yum.repos.d 
 sudo wget -O jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
-  sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
- yum install epel-release # repository that provides 'daemonize'
-  yum install java-11-openjdk-devel
-  yum install jenkins
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+yum install epel-release # repository that provides 'daemonize'
+yum install java-11-openjdk-devel
+yum install jenkins
